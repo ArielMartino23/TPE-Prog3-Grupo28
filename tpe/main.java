610438c159784632850ee1155f2b4e59d688e77e
ArielMartino23 <70241517+ArielMartino23@users.noreply.github.com>
2025-05-09 16:12:29 -0300

    leimos el csv e imprimos piezas y maquinas en el main

--- tpe/main.java
+++ tpe/main.java
@@ -1,5 +1,18 @@
 package tpe;
-public class main {
-    /*algo */
+import java.io.File;
+import java.util.*;
 
+public class Main {
+    private static HashMap<String, Maquina> maquinas;
+    private static int piezas;
+
+    public static void main(String[] args){
+        Lector lector = new Lector("tpe/datasets/estructura.csv");
+
+        maquinas = lector.obtenerMaquinas();
+        piezas = lector.obtenerPiezas();
+
+        System.out.println("las piezas son: "+piezas);
+        System.out.println("las maquinas son: "+maquinas);
+    }
 }
