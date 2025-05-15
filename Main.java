3ee639258fe8a38a6f021b31cdf68f963f8fccda
Manuel42-code <manuel.c2893@gmail.com>
2025-05-14 22:52:41 -0300

    traje el repo creo que por vez definitiva

---
+++ Main.java
@@ -0,0 +1,18 @@
+package tpe;
+import java.io.File;
+import java.util.*;
+
+public class Main {
+    private static HashMap<String, Maquina> maquinas;
+    private static int piezas;
+
+    public static void main(String[] args){
+        Lector lector = new Lector("datasets/estructura.csv");
+
+        maquinas = lector.obtenerMaquinas();
+        piezas = lector.obtenerPiezas();
+
+        System.out.println("las piezas son: "+piezas);
+        System.out.println("las maquinas son: "+maquinas);
+    }
+}
---
+++ Pieza.java
@@ -0,0 +1,17 @@
+package tpe;
+import java.util.*;
+public class Pieza {
+    int numeroPiezas;
+
+    public Pieza(int numeroPiezas) {
+        this.numeroPiezas = numeroPiezas;
+    }
+
+    public int getNumeroPiezas() {
+        return numeroPiezas;
+    }
+
+    public void setNumeroPiezas(int numeroPiezas) {
+        this.numeroPiezas = numeroPiezas;
+    }
+}
